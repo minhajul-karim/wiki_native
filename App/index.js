39505d265ef4d89fe0bb3a0c8a92d0056af39943
minhajul-karim <minhajul.kaarim@gmail.com>
2020-11-04 19:18:20 +0600

    Add icons in tab route and change header title based on route

--- App/index.js
+++ App/index.js
@@ -1,9 +1,13 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { NavigationContainer } from '@react-navigation/native'
+import {
+  NavigationContainer,
+  getFocusedRouteNameFromRoute,
+} from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import React from 'react'
 import { Text, View } from 'react-native'
 import { Provider as PaperProvider } from 'react-native-paper'
+import { Ionicons } from '@expo/vector-icons'
 
 import DetailScreen from './screens/DetailScreen'
 import HomeScreen from './screens/HomeScreen'
@@ -23,7 +27,21 @@ function CreateEntryScreen() {
 
 const HomeTabs = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName
+          if (route.name === 'Home') {
+            iconName = 'ios-home'
+          } else if (route.name === 'Random') {
+            iconName = 'md-shuffle'
+          } else if (route.name === 'Create') {
+            iconName = 'md-create'
+          }
+          return <Ionicons name={iconName} size={size} color={color} />
+        },
+      })}
+    >
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Random" component={RandomScreen} />
       <Tab.Screen name="Create" component={CreateEntryScreen} />
@@ -31,12 +49,32 @@ const HomeTabs = () => {
   )
 }
 
+// Get header title from tab route
+const getHeaderTitle = (route) => {
+  // Define route name for the initial screen
+  const routeName = getFocusedRouteNameFromRoute(route) ?? 'Home'
+  switch (routeName) {
+    case 'Home':
+      return 'All Entries'
+    case 'Random':
+      return 'Random Page'
+    default:
+      return 'Create Page'
+  }
+}
+
 export default function App() {
   return (
     <PaperProvider>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name="HomeTabs" component={HomeTabs} />
+          <Stack.Screen
+            name="HomeTabs"
+            component={HomeTabs}
+            options={({ route }) => ({
+              headerTitle: getHeaderTitle(route),
+            })}
+          />
           <Stack.Screen
             name="detailScreen"
             component={DetailScreen}
