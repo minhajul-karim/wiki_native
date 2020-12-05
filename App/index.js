f2e1e46f5f1abb2ea0bed094d0c0512548bf5dd8
minhajul-karim <minhajul.kaarim@gmail.com>
2020-12-05 15:47:30 +0600

    Use stack navigation for each tab screens

--- App/index.js
+++ App/index.js
@@ -8,14 +8,16 @@ import HomeScreen from './screens/HomeScreen'
 import NewPostScreen from './screens/NewPostScreen'
 import RandomScreen from './screens/RandomScreen'
 
-const Stack = createStackNavigator()
-
 const HomeStack = createStackNavigator()
 
 const HomeStackScreen = () => {
   return (
     <HomeStack.Navigator>
-      <HomeStack.Screen name="Home" component={HomeScreen} />
+      <HomeStack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ title: 'Wiki' }}
+      />
       <HomeStack.Screen name="Details" component={DetailScreen} />
     </HomeStack.Navigator>
   )
@@ -26,7 +28,11 @@ const RandomStack = createStackNavigator()
 const RandomStackScreen = () => {
   return (
     <RandomStack.Navigator>
-      <RandomStack.Screen name="Random" component={RandomScreen} />
+      <RandomStack.Screen
+        name="Random"
+        component={RandomScreen}
+        options={{ title: 'Random Post' }}
+      />
       <RandomStack.Screen name="Details" component={DetailScreen} />
     </RandomStack.Navigator>
   )
@@ -48,48 +54,6 @@ const NewPostStackScreen = ({ navigation }) => {
     </NewPostStack.Navigator>
   )
 }
-// const HomeTabs = () => {
-//   return (
-//     <Tab.Navigator
-//       shifting
-//       tabBarOptions={{
-//         keyboardHidesTabBar: true,
-//       }}
-//       barStyle={{ backgroundColor: '#fff' }}
-//       activeColor="#2A78C7"
-//     >
-//       <Tab.Screen
-//         name="Home"
-//         component={HomeScreen}
-//         options={{ tabBarIcon: 'home' }}
-//       />
-//       <Tab.Screen
-//         name="Random"
-//         component={RandomScreen}
-//         options={{ tabBarIcon: 'dice-5' }}
-//       />
-//       <Tab.Screen
-//         name="New"
-//         component={NewPostScreen}
-//         options={{ tabBarIcon: 'file-plus' }}
-//       />
-//     </Tab.Navigator>
-//   )
-// }
-
-// Get header title from tab route
-// const getHeaderTitle = (route) => {
-//   // Define route name for the initial screen
-//   const routeName = getFocusedRouteNameFromRoute(route) ?? 'Home'
-//   switch (routeName) {
-//     case 'Home':
-//       return 'Wiki'
-//     case 'Random':
-//       return 'Random Post'
-//     default:
-//       return 'New Post'
-//   }
-// }
 
 const Tab = createMaterialBottomTabNavigator()
 
@@ -113,32 +77,11 @@ export default function App() {
             options={{ tabBarIcon: 'dice-5' }}
           />
           <Tab.Screen
-            name="NewPost"
+            name="New"
             component={NewPostStackScreen}
             options={{ tabBarIcon: 'file-plus' }}
           />
         </Tab.Navigator>
-        {/* <Stack.Navigator
-          screenOptions={{
-            headerStyle: { backgroundColor: '#fff' },
-            headerTintColor: '#000',
-          }}
-        >
-          <Stack.Screen
-            name="HomeTabs"
-            component={HomeTabs}
-            options={({ route }) => ({
-              headerTitle: getHeaderTitle(route),
-            })}
-          />
-          <Stack.Screen
-            name="detailScreen"
-            component={DetailScreen}
-            options={({ route }) => ({
-              title: makeTitleCase(route.params.title),
-            })}
-          />
-        </Stack.Navigator> */}
       </NavigationContainer>
     </PaperProvider>
   )
